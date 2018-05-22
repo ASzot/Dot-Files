c3dd57c123494201cb18759c4840164ae51c3a28
Andrew Szot <me@andrewszot.com>
2018-05-22 15:05:35 -0700

    added mac pull

---
+++ mac_pull.sh
@@ -0,0 +1,14 @@
+rm -rf ~/.vim
+cp -R ./.vim ~/.vim
+
+rm ~/.zshrc
+cp .zshrc ~/.zshrc
+
+rm ~/.tmux.conf
+cp .tmux.conf ~/.tmux.conf
+
+rm ~/.gitconfig
+cp .gitconfig ~/.gitconfig
+
+rm -rf ~/.kwm
+cp -R ./.kwm ~/.kwm
