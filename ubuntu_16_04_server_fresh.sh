b13b8b42bc099c3b88ab49f8ab906fdc0248622d
caispp <me@andrewszot.com>
2018-10-03 13:01:31 -0700

    Added server install script

---
+++ ubuntu_16_04_server_fresh.sh
@@ -0,0 +1,15 @@
+sudo apt-get install -y vim
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential
+cd /tmp
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install nvidia-390
+
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install -y cuda
