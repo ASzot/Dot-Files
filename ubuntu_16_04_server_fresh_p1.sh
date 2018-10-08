b2a2279f4004084421c771796d12c6cd84a4de1b
Andrew Szot <me@andrewszot.com>
2018-10-08 15:22:38 -0700

    Updated server install scripts

--- ubuntu_16_04_server_fresh_p1.sh
+++ ubuntu_16_04_server_fresh_p1.sh
@@ -1,3 +1,8 @@
+# Settings related setup
+sudo apt-get install tmux
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 # Note that you must have cudnn in the /tmp folder
 sudo apt-get install -y vim
 sudo apt-get update
@@ -9,5 +14,10 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 sudo apt install nvidia-390
 
-# YOU NEED TO REBOOT NOW
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install -y cuda
 
+# YOU NEED TO REBOOT NOW
--- ubuntu_16_04_server_fresh_p2.sh
+++ ubuntu_16_04_server_fresh_p2.sh
@@ -1,16 +1,4 @@
-# NVIDIA DRIVERS NEED TO BE INSTALLED AND COMPUTER REBOOTED
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt-get update
-sudo apt-get install -y cuda
-
-# Settings related setup
-sudo apt-get install tmux
-sudo apt-get install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sh ~/.dot-files/update.sh
-
 
 # Add proper paths 
 cat <<EOF >> ~/.dot-files/set_env_vars.sh
@@ -18,6 +6,8 @@ export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=\${CUDA_HOME}/lib64
 export PATH=\${CUDA_HOME}/bin:\${PATH}
 EOF
+
+source ~/.zshrc
 
 cd /tmp
 tar xvzf cudnn-9.0-linux-x64-v7.3.1.20.tgz
@@ -32,11 +22,11 @@ sudo apt-get install -y ffmpeg
 # Install conda now 
 sudo chmod -R 777 /opt/
 
-# Specify /opt/conda in the installation.
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 cat <<EOF >> ~/.dot-files/set_env_vars.sh
 export PATH=/opt/conda/bin:$PATH
 EOF
 
 source ~/.zshrc
 
+# Specify /opt/conda in the installation.
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
