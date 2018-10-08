507b4c0bc8811cbf68e86cdb182a90f30f7f1f40
caispp <me@andrewszot.com>
2018-10-08 13:59:02 -0700

    Added server config settings

--- ubuntu_16_04_server_fresh.sh
+++ ubuntu_16_04_server_fresh.sh
@@ -1,3 +1,4 @@
+# Note that you must have cudnn in the /tmp folder
 sudo apt-get install -y vim
 sudo apt-get update
 sudo apt-get upgrade
@@ -13,3 +14,9 @@ sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install -y cuda
+
+# Settings related setup
+sudo apt-get install tmux
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
