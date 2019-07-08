124d7a8975e05a0a460b4d8484c2c215f89f16ac
ASzot <me@andrewszot.com>
2019-07-08 14:17:00 -0700

    removed uncessary from linux setup script

--- ubuntu_16_04_fresh.sh
+++ ubuntu_16_04_fresh.sh
@@ -4,7 +4,6 @@ sudo apt-get -y install tmux
 sudo apt-get -y install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sh pull.sh
-
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -P ~/
-sudo apt-get install build-essential
-sudo apt-get install exuberant-ctags
+#sudo apt-get install build-essential
+#sudo apt-get install exuberant-ctags
