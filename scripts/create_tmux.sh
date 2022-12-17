20336547c38a5430f413032911d42b9a1f9751b5
ASzot <me@andrewszot.com>
2022-12-16 19:30:05 -0800

    Updated scripts

--- scripts/create_tmux.sh
+++ scripts/create_tmux.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+# Usage: setup_tmux.sh session_name start_path conda_env
+# Conda env can be "" for no conda env.
 
-echo "${NAMES[i]}"
-tmux has-session -t "${NAMES[i]}" &> /dev/null
+tmux has-session -t "$1" &> /dev/null
 
 if [ $? != 0 ] 
 then
-	tmux new-session -s "${NAMES[i]}" -d
-	tmux send-keys -t "${NAMES[i]}" "cd ${DIRS[i]} ; conda activate ${CONDA_ENVS[i]} ; nvim ." C-m 
-	tmux split-window -t "${NAMES[i]}" -h
-	tmux send-keys -t "${NAMES[i]}" "cd ${DIRS[i]} ; conda activate ${CONDA_ENVS[i]}" C-m 
-	tmux split-window -t "${NAMES[i]}" -v -l "15%"
+	tmux new-session -s "$1" -d
+	tmux send-keys -t "$1" "cd $2 ; conda activate $3 ; nvim ." C-m 
+	tmux split-window -t "$1" -h
+	tmux send-keys -t "$1" "cd $2 ; conda activate $3" C-m 
+	tmux split-window -t "$1" -v -l "15%"
 fi
 
--- scripts/setup_tmux.sh
+++ scripts/setup_tmux.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-NAMES=("hablab" "rlt" "math" "me")
-DIRS=("~/Documents/code/habitat-lab" "~/Documents/code/hablab_exp" "~/me" "~/me")
-CONDA_ENVS=("hablab" "hablabexp" "" "")
+NAMES=("hablab" "rlt" "me")
+DIRS=("~/Documents/code/habitat-lab" "~/Documents/code/hablab_exp" "~/me")
+CONDA_ENVS=("hablab" "hablabexp" "")
 
 for i in "${!NAMES[@]}"
 do
-	echo "${NAMES[i]}"
-	tmux has-session -t "${NAMES[i]}" &> /dev/null
-
-	if [ $? != 0 ] 
-	then
-		tmux new-session -s "${NAMES[i]}" -d
-		tmux send-keys -t "${NAMES[i]}" "cd ${DIRS[i]} ; conda activate ${CONDA_ENVS[i]} ; nvim ." C-m 
-		tmux split-window -t "${NAMES[i]}" -h
-		tmux send-keys -t "${NAMES[i]}" "cd ${DIRS[i]} ; conda activate ${CONDA_ENVS[i]}" C-m 
-		tmux split-window -t "${NAMES[i]}" -v -l "15%"
-	fi
+	MYDIR="$(dirname "$(realpath "$0")")"
+	sh $MYDIR/create_tmux.sh ${NAMES[i]} ${DIRS[i]} ${CONDA_ENVS[i]}
 done
 
