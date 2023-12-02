c0e4a9ffc76be280c09ee740e3fe3a978ce9cf78
ASzot <ASzot@users.noreply.github.com>
2023-12-02 06:20:09 -0800

    Upgraded daily scripts

--- cron/daily.sh
+++ cron/daily.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Push wiki
-cd ~/me && git add -A && git commit -m "Daily update" && git push
-
 # Clean up downloads folder.
 find ~/Downloads/* -maxdepth 1 -mtime +1 -exec rm -rf {} \;
 
@@ -10,20 +7,26 @@ find ~/Downloads/* -maxdepth 1 -mtime +1 -exec rm -rf {} \;
 # Fetch the BeFocused data
 # open -n /Users/andrewszot/Library/Mobile\ Documents/com\~apple\~Automator/Documents/export_befocused_csv.app
 
-rm ~/Downloads/daily/*
+# Get daily PDFs
+rm ~/me/daily_pdfs/*
 cd ~/Downloads
-
-sh ~/me/math/analysis/generate_pdfs.sh
-# Generate 
 /Users/andrewszot/miniconda3/bin/python ~/.dot-files/cron/get_daily.py
 
+# Update the core PDFs
+sh ~/me/math/analysis/generate_pdfs.sh
+sh ~/me/research_analysis/generate_pdfs.sh
+
 # Remove the auxiliary latex files.
-rm ~/Downloads/daily/*.aux
-rm ~/Downloads/daily/*.out
-rm ~/Downloads/daily/*.log
+for folderName in "daily_pdfs" "core_pdfs" "research" "research_analysis" "math_encyclopedia"
+do
+	rm ~/me/$folderName/*.aux
+	rm ~/me/$folderName/*.out
+	rm ~/me/$folderName/*.log
+	rm ~/me/$folderName/*.fls
+done
 
-# Generate stats vis.
-# /Users/andrewszot/miniconda3/bin/python ~/.dot-files/cron/vis_stats.py
+# Push wiki
+cd ~/me && git add -A && git commit -m "Daily update" && git push
 
-open ~/Downloads/daily
+open ~/me/daily_pdfs/
 
--- cron/get_daily.py
+++ cron/get_daily.py
@@ -3,24 +3,28 @@ import os.path as osp
 import random
 
 home_dir = osp.expanduser("~")
-write_dir = osp.join(home_dir, "Downloads", "daily")
+write_dir = osp.join(home_dir, "me", "daily_pdfs")
 
 os.makedirs(write_dir, exist_ok=True)
 
 root_dir = osp.join(home_dir, "me")
 
-fetch = {"research": 1, "math_encyclopedia": 2, "books": 1}
+fetch = {"research": 2, "math_encyclopedia": 3, "books": 2}
 
 
 for base_dir, count in fetch.items():
     base_dir = osp.join(root_dir, base_dir)
-    all_f = os.listdir(base_dir)
+    all_f = [f for f in os.listdir(base_dir) if f.endswith(".tex")]
 
     for _ in range(count):
         f = random.choice(all_f)
+        print("\n" * 5)
+        print("-" * 10)
         print(f"Trying {f}")
         full_f = osp.join(base_dir, f)
         cmd = f"pdflatex -output-directory {write_dir} {full_f}"
         print("Running", cmd)
 
+        os.system(cmd)
+        # Try twice because if there is an error, you can go in and fix it.
         os.system(cmd)
